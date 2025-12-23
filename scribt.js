f9545a072a62d4cccac14aa122e95bcca3f9dbb6
persnal.cv <omar.23cs263@student.uomosul.edu.iq>
2025-12-23 20:39:52 +0300

    Initial project structure (HTML, CSS, JS files)

--- scribt.js
+++ scribt.js
@@ -1,21 +1,10 @@
 const toggleBtn = document.getElementById("themeToggle");
 const body = document.body;
 
-// 1. عند تحميل الصفحة: تحقق هل المستخدم اختار الوضع الليلي سابقاً؟
-if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark");
-    toggleBtn.textContent = "☀️";
-}
-
 toggleBtn.addEventListener("click", () => {
     body.classList.toggle("dark");
+    body.classList.toggle("light");
 
-    // 2. تحديث الشكل وتخزين الاختيار في ذاكرة المتصفح
-    if (body.classList.contains("dark")) {
-        toggleBtn.textContent = "☀️";
-        localStorage.setItem("theme", "dark"); // حفظ الوضع الليلي
-    } else {
-        toggleBtn.textContent = "🌙";
-        localStorage.setItem("theme", "light"); // حفظ الوضع الفاتح
-    }
+    toggleBtn.textContent = 
+        body.classList.contains("dark") ? "🌞" : "🌙";
 });

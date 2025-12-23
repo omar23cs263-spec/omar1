957856e612aa0c19869869846b50ec74c816c85a
TOTAL TECH CO <omar.23cs263@student.uomosul.edu.iq>
2025-12-23 12:20:09 +0300

    make some change in css

--- scribt.js
+++ scribt.js
@@ -1,20 +1,12 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // Select the button and the hidden div
-    const contactBtn = document.getElementById('contactBtn');
-    const socialLinks = document.getElementById('socialLinks');
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
 
-    // Add click event listener for interactivity
-    contactBtn.addEventListener('click', () => {
-        // Toggle visibility by removing/adding the 'hidden' class
-        if (socialLinks.classList.contains('hidden')) {
-            socialLinks.classList.remove('hidden');
-            contactBtn.textContent = 'Hide Social Media Links';
-        } else {
-            socialLinks.classList.add('hidden');
-            contactBtn.textContent = 'Show Social Media Links';
-        }
-    });
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
 
+    if (body.classList.contains("dark")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
 });
